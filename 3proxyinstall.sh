2ea6a4b1bc5c6a41494c82633e952a7406260ca8
MaxUNof <maxunof@gmail.com>
2018-03-24 15:17:59 +0300

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y build-essential nano libevent-dev libssl-dev
-wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.11.tar.gz
+wget --no-check-certificate https://github.com/MaxUNof/3proxy/blob/master/3proxy-0.8.11.tar.gz
 tar xzf 3proxy-0.8.11.tar.gz
 cd 3proxy-0.8.11
 make -f Makefile.Linux
@@ -8,11 +8,11 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/MaxUNof/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/MaxUNof/3proxy/raw/master/.users
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/barankilic/3proxy/master/3proxyinit
+wget --no-check-certificate https://raw.github.com/MaxUNof/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
