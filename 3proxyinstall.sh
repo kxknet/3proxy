05831f0d52ab303c303422861ad33615a4a84804
MaxUNof <maxunof@gmail.com>
2018-03-24 16:44:01 +0300

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y build-essential nano libevent-dev libssl-dev
-wget --no-check-certificate hhttp://3proxy.ru/0.8.11/3proxy-0.8.11.tgz
+wget --no-check-certificate http://3proxy.ru/0.8.11/3proxy-0.8.11.tgz
 tar zxvf 3proxy-0.8.11.tar.gz
 cd 3proxy-0.8.11
 cd src
